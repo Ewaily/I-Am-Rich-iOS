c204b60acf5ac3ea5de03e32e7504ec0491e426a
Muhammad Ewaily <muhammadewaily@Ewailys-Mac.local>
2018-12-21 14:22:05 +0200

    Project Completed

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Muhammad Ewaily on 9/24/18.
+//  Copyright © 2018 Muhammad Ewaily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
